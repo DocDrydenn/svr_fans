3bda93b7d141342e2721e37b13ecbc972ce41d23
Docdrydenn <docdrydenn@gmail.com>
2022-01-21 21:51:25 -0500

    v2.7

--- svr_fans.sh
+++ svr_fans.sh
@@ -91,9 +91,9 @@ flags() {
   ([ "$2" = "h" ] || [ "$2" = "-h" ]) && usage_example
   ([ "$3" = "h" ] || [ "$3" = "-h" ]) && usage_example
 
-  ([ "${#1}" > 5 ]) && CONF=$1
-  ([ "${#2}" > 5 ]) && CONF=$2
-  ([ "${#3}" > 5 ]) && CONF=$3
+  if ${#1} > 5; then; CONF=$1; fi
+  if ${#2} > 5; then; CONF=$2; fi
+  if ${#3} > 5; then; CONF=$3; fi
 
   ([ "${#1}" < 5 ]) && SPEED=$1
   ([ "${#2}" < 5 ]) && SPEED=$2
